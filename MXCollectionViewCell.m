13430b796bea6b29da0f874546aae25ceabfca7d
张全鹏 <274430533@qq.com>
2016-04-11 22:39:21 +0800

    delete useless code

--- MXCollectionViewCell.m
+++ MXCollectionViewCell.m
@@ -7,7 +7,6 @@
 {
     if (self = [super initWithFrame:frame])
     {
-
         _imageView = [[UIImageView alloc] initWithFrame:self.contentView.bounds];
         [self.contentView addSubview:_imageView];
     }
